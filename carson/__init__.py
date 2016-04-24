4615a9e26f9a6064572d409ccf8a79a7ab584a38
Nathan Merritt <gnm@the-merritts.net>
2016-04-24 15:12:12 -0400

    Allow overwriting of config from envvar

--- carson/__init__.py
+++ carson/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('carson.default_settings')
+app.config.from_envvar('CARSON_SETTINGS', silent=True)
 
 db = SQLAlchemy(app)
 
