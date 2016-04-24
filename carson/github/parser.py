d888efcb20a2447d4a1bed2a1a07d5b57c3bedde
Sam Roberts <samgqroberts@gmail.com>
2016-04-24 14:50:44 -0400

    github parser parses sha, github_username, and timestamp

--- carson/github/parser.py
+++ carson/github/parser.py
@@ -1,6 +1,17 @@
+import requests
+
 class Parser(object):
-    def parse(self, message):
+
+    def get_pull_request_data(self, url):
+        return requests.get(url).json()
+
+    def parse_comment_message(self, comment_message):
+        pull_request_api_url = comment_message['issue']['pull_request']['url']
+        pull_request_data = self.get_pull_request_data(pull_request_api_url)
         parsed = {
-          'issue_number': message['issue']['number']
+            'sha': pull_request_data['merge_commit_sha'],
+            'github_username': comment_message['sender']['login'],
+            'timestamp': comment_message['comment']['created_at'],
+            'issue_number': comment_message['issue']['number'],
         }
         return parsed
--- tests/test_github_parser.py
+++ tests/test_github_parser.py
@@ -4,8 +4,20 @@ import json
 from carson.github.parser import Parser
 
 class GithubParserTest(unittest.TestCase):
+
+    def mock_get_pull_request_data(self, url):
+        self.assertEqual(url, 'https://api.github.com/repos/SylverStudios/carson/pulls/2')
+        return json.loads(
+            open(os.path.join(os.path.dirname(__file__), 'test_github_pull_api_get.json')).read()
+        )
+
     def test_hello(self):
-        json_data=open(os.path.join(os.path.dirname(__file__), 'test_github_message.json')).read()
+        json_data=open(os.path.join(os.path.dirname(__file__), 'test_github_comment_event.json')).read()
         data = json.loads(json_data)
-        parsedData = Parser().parse(data)
+        parser = Parser()
+        parser.get_pull_request_data = self.mock_get_pull_request_data
+        parsedData = parser.parse_comment_message(data)
+        self.assertEqual(parsedData['sha'], 'a5b6485a079091efbe6d5aa5122acec1dde02df4')
+        self.assertEqual(parsedData['github_username'], 'samgqroberts')
+        self.assertEqual(parsedData['timestamp'], '2016-04-24T17:40:32Z')
         self.assertEqual(parsedData['issue_number'], 2)
