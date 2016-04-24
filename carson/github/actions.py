fd511736f86324a339f4df307b7fe4d0b9aa06df
Nathan Merritt <gnm@the-merritts.net>
2016-04-24 13:18:47 -0400

    Placeholder pr merge action

---
+++ carson/github/actions.py
@@ -0,0 +1,8 @@
+
+class MergeAction(object):
+    """Action that merges a pull request via the SHA of its head commit"""
+    def __init__(self, appointment):
+        self.appointment = appointment
+
+    def run(self):
+        pass  # TODO :-)
