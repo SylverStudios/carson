9995babfe7bce5a0b6bd98316a89086bd0e8a03f
Sam Roberts <samgqroberts@gmail.com>
2016-04-24 16:12:37 -0400

    GithubParser: check for command in comment body

--- carson/github/parser.py
+++ carson/github/parser.py
@@ -5,15 +5,29 @@ class Parser(object):
     def get_pull_request_data(self, url):
         return requests.get(url).json()
 
+    def discern_condition_and_actions(self, comment_body):
+        condition_action_pairings = [];
+        if "carson matp" in comment_body:
+            condition_action_pairings.append({
+                'condition': 'passed',
+                'actions': ['merge', 'notify_slack'],
+            })
+        return condition_action_pairings
+
     def parse_comment_message(self, comment_message):
+        # need to get pull request information to (another API call)
         pull_request_api_url = comment_message['issue']['pull_request']['url']
         pull_request_data = self.get_pull_request_data(pull_request_api_url)
+        # parse comment body for event information
+        comment_body = comment_message['comment']['body']
+        # compile and return relevant information
         parsed = {
-            'sha': pull_request_data['head']['sha'],
-            'github_username': comment_message['sender']['login'],
+            'commit_sha': pull_request_data['head']['sha'],
+            'gh_username': comment_message['sender']['login'],
             'timestamp': comment_message['comment']['created_at'],
-            'issue_number': comment_message['issue']['number'],
+            'pr_number': comment_message['issue']['number'],
             'repo': comment_message['repository']['full_name'],
             'base_branch': pull_request_data['base']['ref'],
+            'condition_action_pairings': self.discern_condition_and_actions(comment_body),
         }
         return parsed
--- tests/test_github_parser.py
+++ tests/test_github_parser.py
@@ -11,15 +11,42 @@ class GithubParserTest(unittest.TestCase):
             open(os.path.join(os.path.dirname(__file__), 'test_github_pull_api_get.json')).read()
         )
 
-    def test_hello(self):
-        json_data=open(os.path.join(os.path.dirname(__file__), 'test_github_comment_event.json')).read()
-        data = json.loads(json_data)
+    def get_parser_instance(self):
         parser = Parser()
-        parser.get_pull_request_data = self.mock_get_pull_request_data
-        parsedData = parser.parse_comment_message(data)
-        self.assertEqual(parsedData['sha'], '7753bb8a60c85105636aaa176c830475b3fa1586')
-        self.assertEqual(parsedData['github_username'], 'samgqroberts')
-        self.assertEqual(parsedData['timestamp'], '2016-04-24T17:40:32Z')
-        self.assertEqual(parsedData['issue_number'], 2)
-        self.assertEqual(parsedData['repo'], 'SylverStudios/carson')
-        self.assertEqual(parsedData['base_branch'], 'master')
+        Parser().get_pull_request_data = self.mock_get_pull_request_data
+        return parser
+
+    def get_test_comment_event(self):
+        return json.loads(
+            open(os.path.join(os.path.dirname(__file__), 'test_github_comment_event.json')).read()
+        )
+
+    def assert_parsed_data(self, parsed_data):
+        self.assertEqual(parsed_data['commit_sha'], '7753bb8a60c85105636aaa176c830475b3fa1586')
+        self.assertEqual(parsed_data['gh_username'], 'samgqroberts')
+        self.assertEqual(parsed_data['timestamp'], '2016-04-24T17:40:32Z')
+        self.assertEqual(parsed_data['pr_number'], 2)
+        self.assertEqual(parsed_data['repo'], 'SylverStudios/carson')
+        self.assertEqual(parsed_data['base_branch'], 'master')
+
+    def test_parse_comment_matp(self):
+        comment_data = self.get_test_comment_event()
+        comment_data['comment']['body'] = 'blah blah carson matp blah blah'
+        parser = self.get_parser_instance()
+        parsed_data = parser.parse_comment_message(comment_data)
+        self.assert_parsed_data(parsed_data)
+        condition_action_pairings = parsed_data['condition_action_pairings']
+        self.assertEqual(len(condition_action_pairings), 1)
+        pairing = condition_action_pairings[0]
+        self.assertEqual(pairing['condition'], 'passed')
+        self.assertIn('merge', pairing['actions'])
+        self.assertIn('notify_slack', pairing['actions'])
+
+    def test_parse_comment_no_message_encoded(self):
+        comment_data = self.get_test_comment_event()
+        comment_data['comment']['body'] = 'blah blah blah blah'
+        parser = self.get_parser_instance()
+        parsed_data = parser.parse_comment_message(comment_data)
+        self.assert_parsed_data(parsed_data)
+        condition_action_pairings = parsed_data['condition_action_pairings']
+        self.assertEqual(len(condition_action_pairings), 0)
