678204bb1618c75d4aaae2b55cf911568bd47c96
Nathan Merritt <gnm@the-merritts.net>
2016-05-01 13:59:15 -0400

    Made default db sqllite

--- carson/default_settings.py
+++ carson/default_settings.py
@@ -1,6 +1,6 @@
 DEBUG = True
 SECRET_KEY = "development"
-SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://carson:1234@localhost:5432/carson"
+SQLALCHEMY_DATABASE_URI = "sqlite+pysqlite:///tmp/carson.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 GITHUB_API_KEY = "not-checked-in"
 SLACK_API_KEY = "not-checked-in"
