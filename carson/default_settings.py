c5e0bae516f07db95f0e3c0c707bbb4a889f5deb
Nathan Merritt <gnm@the-merritts.net>
2016-05-01 14:06:45 -0400

    Use in-memory sqllite

--- carson/default_settings.py
+++ carson/default_settings.py
@@ -1,6 +1,6 @@
 DEBUG = True
 SECRET_KEY = "development"
-SQLALCHEMY_DATABASE_URI = "sqlite+pysqlite:///tmp/carson.db"
+SQLALCHEMY_DATABASE_URI = "sqlite://"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 GITHUB_API_KEY = "not-checked-in"
 SLACK_API_KEY = "not-checked-in"
